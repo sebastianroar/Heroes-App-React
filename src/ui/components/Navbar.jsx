249670266f04138a408fb1a454bf8e0ce8ab3229
Sebastian Roa <809sebas@gmail.com>
2023-06-25 14:45:36 -0500

    Add logout

--- src/ui/components/Navbar.jsx
+++ src/ui/components/Navbar.jsx
@@ -1,13 +1,20 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth/context/AuthContext";
 
 export const Navbar = () => {
   const navigate = useNavigate();
 
+  const { user, logout } = useContext(AuthContext);
+
   const onLogout = () => {
+    console.log(user);
+    logout();
     navigate("/login", {
       replace: true,
     });
   };
+
   return (
     <nav className="navbar navbar-expand-sm navbar-dark bg-dark p-2">
       <Link className="navbar-brand" to="/">
@@ -47,7 +54,7 @@ export const Navbar = () => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-primary">Sebastian</span>
+          <span className="nav-item nav-link text-primary">{user?.name}</span>
           <button className="nav-item nav-link btn" onClick={onLogout}>
             Logout
           </button>
